fbf48cef58f2d0a34985a1b56c15371d3c7fb6b4
GermanSar <78889433+GermanSar@users.noreply.github.com>
2021-03-05 13:34:19 -0500

    Add files via upload

---
+++ E1/src/main.cpp
@@ -0,0 +1,27 @@
+/*1. Escribe un programa que lea de la entrada estándar dos números y muestre 
+en la salida estándar su suma, resta, multiplicación y división.*/
+
+#include <Arduino.h>
+
+int n1,n2, suma = 0, resta = 0, multiplicacion=0, division=0;
+
+void setup() 
+{
+  	Serial.begin(9600);
+}
+
+void loop() 
+{
+	Serial.println("Digite un numero: "); while(Serial.available()){} n1 = Serial.parseInt();	
+    Serial.println("Digite otro numero: "); while(Serial.available()){} n2 = Serial.parseInt();
+
+	suma = n1 + n2;
+	resta = n1 - n2;
+	multiplicacion = n1 * n2;
+	division = n1 / n2;
+	
+  	Serial.println("La suma es: " + String(suma));
+	Serial.println("La resta es: " + String(resta));
+	Serial.println("La multiplicacion es: " + String(multiplicacion));
+	Serial.println("La division es: " + String(division));
+}
